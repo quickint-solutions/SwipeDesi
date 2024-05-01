afc17f97439756746609e283dd6ef959de3cf950
Neel Patel <neelpatel.6531@gmail.com>
2024-05-01 18:31:52 +0530

    fix

--- src/apiV2/categories.ts
+++ src/apiV2/categories.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getCategories = async () => {
-  const response = await axios.get('/categories');
+  const response = await axios.get('/categories?pageSize=1000');
   return response.data;
 };
