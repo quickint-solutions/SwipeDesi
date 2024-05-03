673e216eb3f09fd9d061d7f81d384431525b2dd4
koshti sagar <koshtisagar7778@gmail.com.com>
2024-05-03 10:56:59 +0530

    Privacy Policy add

--- src/module/pages/Route.tsx
+++ src/module/pages/Route.tsx
@@ -9,6 +9,7 @@ import { login } from '../../apiV2/login';
 import { AuthContext } from '../../context/auth.context';
 import Products from './Products';
 import FAQ from './FAQ';
+import PrivacyPolicy from './PrivacyPolicy';
 import MyAccount from './MyAccountPage';
 import { CartContext } from '../../context/cart.context';
 import logo from '../../images/logo1.jpg';
@@ -576,6 +577,7 @@ const RouteComponent: React.FC = () => {
         <Route path="/products" element={<Products />} />
         <Route path="/my-account" element={<MyAccount />} />
         <Route path="/faq" element={<FAQ />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/home" element={<Home />} />
         <Route path="/shopSingle" element={<ShopSingle />} />
         <Route path="/wishlist" element={<Wishlist />} />
