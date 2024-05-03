7d93ebe414fec85a5d1e5e5ca44204bb5f7d05f6
koshti sagar <koshtisagar7778@gmail.com.com>
2024-05-03 11:23:02 +0530

    terms and conditions add

--- src/module/pages/TermsCondition/index.tsx
+++ src/module/pages/TermsCondition/index.tsx
@@ -56,78 +56,71 @@ export default function TermsCondition() {
           <div className="row justify-content-center">
             <div className="col-md-12">
               <div className="mb-4">
-                <p>
-                  Without clarity, you send a very garbled message out to the Universe. We know that the Law of Attraction says that we will attract
-                  what we focus on, so if we don’t have clarity, we will attract confusion.
-                </p>
+                <h4 className="mb-3 fw-600">Description of Service</h4>
                 <p>
                   The best way is to develop and follow a plan. Start with your goals in mind and then work backwards to develop the plan. What steps
-                  are required to get you to the goals? Make the plan as detailed as possible. Try to visualize and then plan for,{' '}
-                </p>
-                <p>
-                  every possible setback. Commit the plan to paper and then keep it with you at all times. Review it regularly and ensure that every
-                  step takes you closer to your Vision and Goals. If the plan doesn’t support the vision then change it!
+                  are required to get you to the goals? Make the plan as detailed as possible. Try to visualize and then plan for, every possible
+                  setback.{' '}
                 </p>
               </div>
               <div className="mb-4">
-                <h4 className="mb-3 fw-600">Personal Information</h4>
-                <ul className="list list-unstyled mb-3">
-                  <li>
-                    <i className="bi bi-check-circle-fill"></i> The truth about success
-                  </li>
-                  <li>
-                    <i className="bi bi-check-circle-fill"></i> If success is a process with a number of defined steps,{' '}
-                  </li>
-                  <li>
-                    <i className="bi bi-check-circle-fill"></i> Then it is just like any other process.
-                  </li>
-                  <li>
-                    <i className="bi bi-check-circle-fill"></i> The first action is always in making the decision to proceed.{' '}
-                  </li>
-                  <li>
-                    <i className="bi bi-check-circle-fill"></i> This is a fundamental step, which most people overlook.
-                  </li>
-                </ul>
+                <h4 className="mb-3 fw-600">Your Registration Obligations</h4>
                 <p>
-                  We also know those epic stories, those modern-day legends surrounding the early failures of such supremely successful folks as
-                  Michael Jordan and Bill Gates. We can look a bit further back in time to Albert Einstein or even further back to Abraham Lincoln.
-                  What made each of these people so successful? Motivation.
+                  Along with your plans, you should consider developing an action orientation that will keep you motivated to move forward at all
+                  times. This requires a little self-discipline, but is a crucial component to achievement of any kind. Before starting any new
+                  activity, ask yourself if that activity will move you closer to your goals. If the answer is no, you may want to reconsider doing it
+                  at that time.
                 </p>
               </div>
               <div className="mb-4">
-                <h4 className="mb-3 fw-600">Use of User Information.</h4>
+                <h4 className="mb-3 fw-600">User Account, Password, and Security</h4>
                 <p>
-                  The first thing to remember about success is that it is a process – nothing more, nothing less. There is really no magic to it and
-                  it’s not reserved only for a select few people. As such, success really has nothing to do with luck, coincidence or fate. It really
-                  comes down to understanding the steps in the process and then executing on those steps.
+                  Commitment is something that comes from understanding that everything has its price and then having the willingness to pay that
+                  price.
                 </p>
               </div>
-              <div>
-                <h4 className="mb-3 fw-600">Disclosure of User Information.</h4>
+              <div className="mb-4">
+                <h4 className="mb-3 fw-600">User Conduct</h4>
                 <p>
-                  There are basically six key areas to higher achievement. Some people will tell you there are four while others may tell you there
-                  are eight. One thing for certain though, is that irrespective of the number of steps the experts talk about, they all originate from
-                  the same roots.<a href="#"> support@example.com</a>{' '}
+                  Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he
+                  decided to focus on arriving at moral perfection. He made a list of 13 virtues, assigning a page to each. Under each virtue he wrote
+                  a summary that gave it fuller meaning. Then he practiced each one for a certain length of time. To make these virtues a habit,{' '}
                 </p>
                 <ul className="list list-unstyled mb-3">
                   <li>
-                    <i className="bi bi-check-circle-fill"></i> You are going on a cruise, but when the ship sets sail,{' '}
+                    <i className="fas fa-check"></i> The truth about success{' '}
                   </li>
                   <li>
-                    <i className="bi bi-check-circle-fill"></i> The sad thing is the majority of people have no clue about what they truly want.{' '}
+                    <i className="fas fa-check"></i> If success is a process with a number of defined steps,{' '}
                   </li>
                   <li>
-                    <i className="bi bi-check-circle-fill"></i> Once you have a clear understanding of what you want,
+                    <i className="fas fa-check"></i> Then it is just like any other process.{' '}
                   </li>
                   <li>
-                    <i className="bi bi-check-circle-fill"></i> Focus is having the unwavering attention to complete what you set out to do.
+                    <i className="fas fa-check"></i> The first action is always in making the decision to proceed.
+                  </li>
+                  <li>
+                    <i className="fas fa-check"></i> This is a fundamental step, which most people overlook.
                   </li>
                 </ul>
-                <p className="mb-0">
-                  Making a decision to do something – this is the first step. We all know that nothing moves until someone makes a decision. The first
-                  action is always in making the decision to proceed. This is a fundamental step, which most people overlook.
-                  <a href="#"> support@example.com</a>
+              </div>
+              <div className="mb-4">
+                <h4 className="mb-3 fw-600">International Use</h4>
+                <p>
+                  Acres of Diamonds… you’ve read the famous story, or at least had it related to you. A farmer hears tales of diamonds and begins
+                  dreaming of vast riches. He sells his farm and hikes off over the horizon, never to be heard from again. Rumors say that years later
+                  he died destitute, never having found the diamonds he spent his life seeking. Meanwhile, the man who bought that farm found a large
+                  and “interesting looking” stone in a stream that ran through the property. He put the stone on his mantle where a visitor recognized
+                  the large stone as a rough diamond. It turned out to be the Hope Diamond.
                 </p>
+              </div>
+              <div className="mb-4">
+                <a href="#" className="btn btn-primary mt-3">
+                  Accept
+                </a>
+                <a href="#" className="btn btn-secondary mt-3">
+                  Close
+                </a>
               </div>
             </div>
           </div>
