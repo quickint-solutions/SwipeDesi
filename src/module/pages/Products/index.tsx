f3a29d684349e66a3ea0d63f3367b335726571a8
Neel Patel <neelpatel.6531@gmail.com>
2024-05-05 14:39:27 +0530

    fix issues

--- src/module/pages/Products/index.tsx
+++ src/module/pages/Products/index.tsx
@@ -81,7 +81,7 @@ export default function Products() {
                         {categoriesData.map((category: any) => {
                           const subCategories = categoriesList?.result?.filter((i: any) => i.parentCategory?._id === category._id) || [];
                           const totalItems = subCategories?.length
-                            ? subCategories.reduce((acc: any, curr: any) => acc + curr.itemCount, 0)
+                            ? subCategories.reduce((acc: any, curr: any) => acc + curr.itemCount, 0) + category.itemCount
                             : category.itemCount;
 
                           return (
