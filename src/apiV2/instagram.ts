e0bb9267d141ab20cfe20446b5aaa28a5c95646d
Koshti Sagar <107100610+sagar7778@users.noreply.github.com>
2024-05-14 11:06:43 +0530

    instagram key change

--- src/apiV2/instagram.ts
+++ src/apiV2/instagram.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getInstagram = async () => {
   const response = await axios.get(
-    'https://graph.instagram.com/me/media?fields=thumbnail_url&access_token=IGQWROWFhXMUdLYWJveUJIV3B1ZAEZAKOGpyQmZAjNHdYSUw2cmxGcVJxcHNJOEpHam1xQ1EtTnpUdmxKSzZA3VVJvTXNLTmdIeTNET1psNG1qN1J0VVROLXl1SmVsVUd4NTFiYXlzSWtOSC0tRTZACTGFxN05HTklPbUUZD',
+    'https://graph.instagram.com/me/media?fields=thumbnail_url&access_token=IGQWROSFgyLUhJcUZAyeTc4R1E1aksydkxHRGF4bVRaXzZAmeGxObXdraU1iV1dfZAmdMWVpwMldWSXFFaWdibkZAYZAE5qd3FKQ1dyT1F4b0ttQ19ET3BpNzRlZAjZAYLU51MVlMeEZAuOW9RVnhGdUEzOUI3ZAWF5UGxORWsZD',
   );
   return response.data;
 };
