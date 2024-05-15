4f555156476a804583c77cfaac1af971c6f02791
Neel Patel <neelpatel.6531@gmail.com>
2024-05-15 22:21:40 +0530

    add hover effect

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { CartContext } from '../context/cart.context';
 import { useMutation } from 'react-query';
 import { addWishList } from '../apiV2/wishlist';
 import { AuthContext } from '../context/auth.context';
 import { useNavigate } from 'react-router-dom';
+import $ from 'jquery';
 
 export default function ProductItem({ product, large }: { product: any; large?: boolean }) {
   const { addItem, isItemInCart } = useContext(CartContext);
@@ -31,8 +32,19 @@ export default function ProductItem({ product, large }: { product: any; large?: 
     },
   });
 
+  useEffect(() => {
+    $(`#product-${product?._id}`).hover(
+      function () {
+        $(`#product-img-${product?._id}`).attr('src', product?.images?.[1] || product?.images?.[0]);
+      },
+      function () {
+        $(`#product-img-${product?._id}`).attr('src', product?.images?.[0]);
+      },
+    );
+  });
+
   return (
-    <div className={`col-xl-${large ? '4' : '3'} col-md-6`} style={{ cursor: 'pointer' }}>
+    <div className={`col-xl-${large ? '4' : '3'} col-md-6`} style={{ cursor: 'pointer' }} id={`product-${product?._id}`}>
       <div className="product">
         {product.discount && (
           <div className="product-label">
@@ -43,7 +55,7 @@ export default function ProductItem({ product, large }: { product: any; large?: 
         <div className="product-image">
           <div className="product-thumb-inner">
             <a href={`/shopSingle?productId=${product?._id}`}>
-              <img className="img-fluid" src={product.images[0]} alt="image" />
+              <img className="img-fluid" id={`product-img-${product?._id}`} src={product.images[0]} alt="image" />
             </a>
           </div>
           <div className="custom-icon">
